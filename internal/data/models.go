27f3386daf410a620fa04347fa92dade34e97782
MuchammadTedyA <78480496+MuchammadTedyA@users.noreply.github.com>
2024-01-18 22:15:10 +0700

    create models.go

---
+++ internal/data/models.go
@@ -0,0 +1,48 @@
+package data
+
+import (
+	"database/sql"
+	"time"
+)
+
+const dbTimeOut = time.Second * 3
+
+var db *sql.DB
+
+func New(dbPool *sql.DB) Models {
+	db = dbPool
+
+	return Models{
+		User:  User{},
+		Token: Token{},
+	}
+}
+
+type Models struct {
+	User  User
+	Token Token
+}
+
+type User struct {
+	ID        int       `json:"id"`
+	UserName  string    `json:"user_name"`
+	Email     string    `json:"email"`
+	FirsName  string    `json:"first_name,omitempty"`
+	LastName  string    `json:"last_name,omitempty"`
+	Password  string    `json:"password"`
+	Active    string    `json:"active"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Token     Token     `json:"token"`
+}
+
+type Token struct {
+	ID        int       `json:"id"`
+	UserName  string    `json:"user_name"`
+	Email     string    `json:"email"`
+	Token     string    `json:"token"`
+	TokenHash []byte    `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Expiry    time.Time `json:"expiry"`
+}
