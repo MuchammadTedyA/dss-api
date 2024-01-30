b7493c8060ba05baceb5691832e5b903657f9f2c
MuchammadTedyA <78480496+MuchammadTedyA@users.noreply.github.com>
2024-01-30 21:09:23 +0700

    Create userHandlers.go
    AllUsers()

---
+++ cmd/api/userHandlers.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"dss-api/internal/data"
+	"net/http"
+)
+
+func (app *application) AllUsers(w http.ResponseWriter, r *http.Request) {
+	var users data.User
+	all, err := users.GetAll()
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: "success",
+		Data:    envelope{"users": all},
+	}
+
+	app.writeJSON(w, http.StatusOK, payload)
+}
