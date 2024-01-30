41e5a89b93f34fbe3014633c9a64e2730226942a
MuchammadTedyA <78480496+MuchammadTedyA@users.noreply.github.com>
2024-01-30 21:39:37 +0700

    Update userHandlers.go
    GetUser()

--- cmd/api/userHandlers.go
+++ cmd/api/userHandlers.go
@@ -3,6 +3,9 @@ package main
 import (
 	"dss-api/internal/data"
 	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func (app *application) AllUsers(w http.ResponseWriter, r *http.Request) {
@@ -20,4 +23,21 @@ func (app *application) AllUsers(w http.ResponseWriter, r *http.Request) {
 	}
 
 	app.writeJSON(w, http.StatusOK, payload)
+}
+
+func (app *application) GetUser(w http.ResponseWriter, r *http.Request) {
+	userID, err := strconv.Atoi(chi.URLParam(r, "id"))
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	user, err := app.models.User.GetOne(userID)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, user)
 }
